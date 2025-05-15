7758004c4bd3d177cd8977aa3e86591d9927933e
AndriyTinkalyuk <tinkalukandriy@gmail.com>
2025-05-15 12:00:09 +0300

    add welcome element

--- Lesson 05/vite-project/src/App.tsx
+++ Lesson 05/vite-project/src/App.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import Greetings from './components/Greetings';
 
+
+const welcomeElement = <h1>Привіт, світ!</h1>;
+const userAge: number = 21;
+const isVisitor: boolean = true;
+
+const welcomeElementUser = <p>Привіт! Тобі {userAge} років!</p>
+const visitorStatusElement = isVisitor ? <p>Ласкаво просимо на наш курс!</p> : <p>Будь ласка, зареєструйтесь для доступу.</p>;
+
 const App = () => {
   return (
     <div>
-      <h1>Welcome to My React App</h1>
-      <Greetings firstName = {"Andrii"} lastName = {"Tinkaluk"}/>
+      <div>{welcomeElement}</div>
+      <Greetings firstName = {"Andrii"} lastName ={"Tinkaluk"}/>
+      {welcomeElementUser}
+      {visitorStatusElement}
     </div>
   );
 };
